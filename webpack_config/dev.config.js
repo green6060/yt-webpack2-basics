58449486ccbe2cd805fc59a6631fbbc4cacb14fd
green6060 <dallas.parker@stgconsulting.com>
2021-03-12 14:19:16 -0700

    seperating production config files from dev config files

--- webpack_config/dev.config.js
+++ webpack_config/dev.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
+  mode: 'development',
 };
